11e04eb2d0398f8d9296acf72f3c23544708f846
rewe999 <54740556+rewe999@users.noreply.github.com>
2020-03-08 17:34:35 +0100

    lab

---
+++ PW/lab2/pesel/Pesel.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PESEL1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Wynik = new System.Windows.Forms.Label();
+            this.Pesel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 123);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Podaj Pesel";
+            // 
+            // Wynik
+            // 
+            this.Wynik.AutoSize = true;
+            this.Wynik.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.Wynik.Location = new System.Drawing.Point(319, 56);
+            this.Wynik.Name = "Wynik";
+            this.Wynik.Size = new System.Drawing.Size(0, 47);
+            this.Wynik.TabIndex = 2;
+            // 
+            // Pesel
+            // 
+            this.Pesel.Location = new System.Drawing.Point(52, 79);
+            this.Pesel.Name = "Pesel";
+            this.Pesel.Size = new System.Drawing.Size(100, 20);
+            this.Pesel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pesel);
+            this.Controls.Add(this.Wynik);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Wynik;
+        private System.Windows.Forms.TextBox Pesel;
+    }
+}
+
