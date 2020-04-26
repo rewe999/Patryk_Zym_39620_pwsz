0ddf2ec940f88ebfdaea78daeb6ccad9d2536fdb
rewe999 <zymek998@gmail.com>
2020-04-26 22:01:01 +0200

    lab6

---
+++ PW/lab6/BackgroundWorkerSample/BackgroundWorkerSample/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BackgroundWorkerSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartAsyncOperation = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ileLiczb = new System.Windows.Forms.TextBox();
+            this.Dzielnik = new System.Windows.Forms.TextBox();
+            this.Od = new System.Windows.Forms.TextBox();
+            this.Do = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartAsyncOperation
+            // 
+            this.btnStartAsyncOperation.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnStartAsyncOperation.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStartAsyncOperation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnStartAsyncOperation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartAsyncOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartAsyncOperation.Location = new System.Drawing.Point(86, 258);
+            this.btnStartAsyncOperation.Name = "btnStartAsyncOperation";
+            this.btnStartAsyncOperation.Size = new System.Drawing.Size(201, 23);
+            this.btnStartAsyncOperation.TabIndex = 0;
+            this.btnStartAsyncOperation.Text = "Rozpocznij operację asynchroniczną";
+            this.btnStartAsyncOperation.UseVisualStyleBackColor = true;
+            this.btnStartAsyncOperation.Click += new System.EventHandler(this.btnStartAsyncOperation_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(49, 192);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(538, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(335, 258);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(200, 22);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Anuluj";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Black;
+            this.lblStatus.Location = new System.Drawing.Point(274, 157);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(69, 23);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Start....";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(44, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ile Liczb";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.Location = new System.Drawing.Point(258, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dzielnik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label3.Location = new System.Drawing.Point(466, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Od";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label4.Location = new System.Drawing.Point(556, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Do";
+            // 
+            // ileLiczb
+            // 
+            this.ileLiczb.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.ileLiczb.Location = new System.Drawing.Point(75, 52);
+            this.ileLiczb.Name = "ileLiczb";
+            this.ileLiczb.Size = new System.Drawing.Size(43, 35);
+            this.ileLiczb.TabIndex = 8;
+            // 
+            // Dzielnik
+            // 
+            this.Dzielnik.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.Dzielnik.Location = new System.Drawing.Point(263, 52);
+            this.Dzielnik.Name = "Dzielnik";
+            this.Dzielnik.Size = new System.Drawing.Size(89, 35);
+            this.Dzielnik.TabIndex = 9;
+            // 
+            // Od
+            // 
+            this.Od.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.Od.Location = new System.Drawing.Point(458, 52);
+            this.Od.Name = "Od";
+            this.Od.Size = new System.Drawing.Size(54, 35);
+            this.Od.TabIndex = 10;
+            // 
+            // Do
+            // 
+            this.Do.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.Do.Location = new System.Drawing.Point(561, 52);
+            this.Do.Name = "Do";
+            this.Do.Size = new System.Drawing.Size(53, 35);
+            this.Do.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label5.Location = new System.Drawing.Point(524, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Do);
+            this.Controls.Add(this.Od);
+            this.Controls.Add(this.Dzielnik);
+            this.Controls.Add(this.ileLiczb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStartAsyncOperation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "BackgroundWorker Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartAsyncOperation;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ileLiczb;
+        private System.Windows.Forms.TextBox Dzielnik;
+        private System.Windows.Forms.TextBox Od;
+        private System.Windows.Forms.TextBox Do;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PW/lab6/BackgroundWorkerSample/BackgroundWorkerSample/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerSample
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker m_oWorker;
+
+        public Form1() {
+            InitializeComponent();
+            m_oWorker = new BackgroundWorker();
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            m_oWorker.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+            m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
+        }
+
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if (e.Cancelled) {
+                lblStatus.Text = "Anulowano.";
+            }
+            else if (e.Error != null) {
+                lblStatus.Text = "Błąd podczas wykonywania operacji w tle.";
+            }
+            else {
+                lblStatus.Text = "Udało się !!!";
+            }
+            btnStartAsyncOperation.Enabled = true;
+            btnCancel.Enabled = false;
+        }
+
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = "W trakcie ......" + progressBar1.Value.ToString() + "%";
+        }
+
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e) {
+            int liczba;
+            float iloscLiczb = Int32.Parse(ileLiczb.Text); 
+            int dzielnik = Int32.Parse(Dzielnik.Text); 
+            int poczPrzedzialu = Int32.Parse(Od.Text); 
+            int konPrzedzialu = Int32.Parse(Do.Text);
+            double progress = 0;
+            int genLiczby = 0;
+
+            while (true) {
+                Thread.Sleep(700);
+                Random rand = new Random();
+                liczba = rand.Next(poczPrzedzialu, konPrzedzialu);
+
+                if (liczba % dzielnik == 0) {
+                    genLiczby++;
+                    progress = genLiczby / iloscLiczb;
+                    progress *= 100;
+
+                    if(progress > 99.9) {
+                        m_oWorker.ReportProgress(100);
+                        return;
+                    }
+                }
+                m_oWorker.ReportProgress((int)progress);
+
+                if (m_oWorker.CancellationPending) {
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+
+            }
+            m_oWorker.ReportProgress(100);
+        }
+
+        private void btnStartAsyncOperation_Click(object sender, EventArgs e)
+        {
+            btnStartAsyncOperation.Enabled  = false;
+            btnCancel.Enabled               = true;
+            m_oWorker.RunWorkerAsync();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+                m_oWorker.CancelAsync();
+            }
+        }
+    }
+}
