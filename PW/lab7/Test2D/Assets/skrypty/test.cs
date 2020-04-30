b2daa28218a052144e56abaeec12faff6264fcf4
rewe999 <zymek998@gmail.com>
2020-04-30 20:04:03 +0200

    lab7

---
+++ PW/lab7/Test2D/Assets/skrypty/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int ilosc = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < ilosc; i++)
+        {
+            print("Patryk " + (i+1));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PW/lab7/Test2D/Assets/skrypty/textview.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textview : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text txt = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        txt.text = "asdasdasd";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
