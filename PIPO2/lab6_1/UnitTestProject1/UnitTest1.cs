b89f0f8a7fb07da4256071480d25223acfd76032
rewe999 <zymek998@gmail.com>
2020-05-05 11:59:25 +0200

    lab 6_1

---
+++ PIPO2/lab6_1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6_1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void silnia()
+        {
+            silnia s = new silnia(0);
+            Assert.AreEqual(s.oblicz(), 1);
+
+            silnia s1 = new silnia(1);
+            Assert.AreEqual(s1.oblicz(), 1);
+
+            silnia s2 = new silnia(5);
+            Assert.AreEqual(s2.oblicz(), 120);
+
+        }
+    }
+}
---
+++ PIPO2/lab6_1/lab6_1/silnia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1 {
+    public class silnia {
+        int n;
+
+        public silnia(int n) {
+            this.n = n;
+        }
+
+        public int oblicz()
+        {
+            int sum = 1;
+            if (n == 0) return 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum *= i;
+            }
+
+            return sum;
+        }
+
+    }
+}
