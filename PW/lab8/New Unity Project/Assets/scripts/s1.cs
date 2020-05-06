b21d455e3bc8660b0c3d24e7f97567438b782164
rewe999 <zymek998@gmail.com>
2020-05-06 15:48:54 +0200

    lab8

---
+++ PW/lab8/New Unity Project/Assets/scripts/s1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+
+    GameObject[] delSphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delSphere = GameObject.FindGameObjectsWithTag("kula");
+        foreach (GameObject del in delSphere) Destroy(del);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
